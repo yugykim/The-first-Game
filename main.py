09271c6d3e7399b1df0167329b068e12d05f4430
yugy <yugyeongk92@gmail.com>
2021-06-09 17:40:48 -0600

    updated more images

--- main.py
+++ main.py
@@ -1,104 +1,69 @@
 import pygame
 from pygame.locals import *
-import random
-
-class Snake:
-    def __init__(self, surface):
-        self.parent_screen = surface
-        self.block = pygame.image.load("resources/ghost.png").convert()
-        self.block = pygame.transform.scale(self.block,(30, 30))
-        self.enemy = pygame.image.load("resources/ghost copy.png").convert()
-        self.enemy = pygame.transform.scale(self.enemy,(30, 30))
-        self.x = 100
-        self.y = 100
-        self.enemy_x = random.randint(0, 450)
-        self.enemy_y = random.randint(50, 150)
-        self.size = 1
-
-
-    def draw(self):
-        self.parent_screen.fill((255, 255, 255))
-        self.parent_screen.blit(self.block, (self.x, self.y))
-        self.enemy_move()
-        pygame.display.flip()
-
-    def move_up(self):
-        self.y -= 10
-        self.draw()
-    
-    def move_down(self):
-        self.y += 10
-        self.draw()
-
-    def move_left(self):
-        self.x -= 10
-        self.draw()
-
-    def move_right(self):
-        self.x += 10
-        self.draw()
-
-    def enemy_draw(self):
-        self.parent_screen.blit(self.enemy, (self.enemy_x, self.enemy_y))
-        pygame.display.flip()
-
-    def enemy_move(self):
-        if self.enemy_x >= 0:
-            enemyX_change = 10
-            self.enemy_x += enemyX_change
-            self.enemy_draw()
-        else:
-            pass
-
-
-            
-
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500, 500))
-        self.snake = Snake(self.surface)
-        self.snake.enemy_draw()
-        self.snake.draw()
+        self.surface = pygame.display.set_mode((550, 550))
+        self.block = pygame.image.load("resources/ghost.png").convert()
+        self.block = pygame.transform.scale(self.block,(30, 30))
+        self.back = pygame.image.load("resources/sky.jpg").convert()
+        self.back = pygame.transform.scale(self.back,(550, 550))
+        self.wall = pygame.image.load("resources/wall.png").convert()
+        self.wall = pygame.transform.scale(self.wall,(150, 30))
+        self.over = pygame.image.load("resources/game-over.png").convert()
+        self.over = pygame.transform.scale(self.over,(550, 550))
+        self.block_over = pygame.image.load("resources/brickwall.png").convert()
+        self.block_over = pygame.transform.scale(self.block_over,(30, 30))
+        self.x = 0
+        self.y = 0
+        self.x_lo = [0, 50, 200, 400, 400]
+        self.y_lo = [100, 130, 250, 280, 300, 350, 400]
+    
+    def draw(self, x_change, y_change):
+        self.x_change = x_change
+        self.y_change = y_change
+        self.x += self.x_change
+        self.y += self.y_change
+        self.surface.blit(self.block, (self.x, self.y))
+        pygame.display.update()
+
+    
+    def wall_im(self):
+        self.surface.blit(self.wall, (0, 100))
+        self.surface.blit(self.wall, (400, 200))
+        self.surface.blit(self.wall, (200, 130))
+        self.surface.blit(self.wall, (0, 250))
+        self.surface.blit(self.wall, (400, 280))
+        self.surface.blit(self.wall, (50, 300))
+        self.surface.blit(self.wall, (300, 350))
+        self.surface.blit(self.wall, (200, 400))
 
     def run(self):
         running = True
-
         while running:
+            self.surface.fill((0,0,0))
+            self.surface.blit(self.back, (0, 0))
+            self.surface.blit(self.block, (self.x, self.y))
+            self.surface.blit(self.block_over, (510, 510))
+            self.wall_im()
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_UP:
-                        self.snake.move_up()
-                        
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
-                        
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
-                        
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
-                        
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
+                        self.draw(0, -10)
+                    elif event.key == K_DOWN:
+                        self.draw(0, 10)
+                    elif event.key == K_LEFT:
+                        self.draw(-10, 0)
+                    elif event.key == K_RIGHT:
+                        self.draw(10, 0)             
+                    elif event.key == K_ESCAPE:
+                        running = False
                 elif event.type == QUIT:
-                    pygame.quit()
-
-
+                    running = False
+            pygame.display.update()
+        pygame.quit()
         
-
-
 if __name__ == '__main__':
     game = Game()
     game.run()
-
-
-
-
-
- 
-
-                
-                
-        
